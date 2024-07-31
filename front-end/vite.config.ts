d7adcc6752c6564bb2ff8b37c77f0b956deaeedd
Giuliana Zhu <135511633+giulianazhu@users.noreply.github.com>
2024-07-31 18:00:32 +0200

    skip-ts-config-attempt-v2

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //below bit to be removed after completing ts
   esbuild: {
     loader: "jsx",
+    include: /src\/.*\.tsx?$/,
+    exclude: [],
   },
 });
